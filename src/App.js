7d2bb282a2ea04599d5bf8d1351678b8949710fd
lakshaysharma2530 <136422387+lakshaysharma2530@users.noreply.github.com>
2024-07-15 12:20:52 +0530

    resolved error 1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <>
     <Home/>
     <Skills/>
+    <div style={{color:"whitesmoke", padding:"5%"}}>
+      <h1>Note:</h1>
+      <p>Thank you for visiting my portfolio website. Please note that the <b>site is currently under development</b>, and some functionalities, such as buttons and interactive elements, may not be fully operational at this time. I am actively working on completing these features and appreciate your understanding and patience.</p>
+      <p>Best regards,</p>
+      <p>Lakshay</p>
+    </div>
     </>
   );
 }
